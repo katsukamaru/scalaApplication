e6851c66da4fe3252ff492e9f6c34fedf6b5d59a
shin katsumaru <ms.katsumaru@gmail.com>
2018-02-28 09:52:07 +0900

    use where clause with slick

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,21 +1,28 @@
 package controllers
 
 import play.api.mvc._
-
 import slick.jdbc.MySQLProfile.api._
+
 import scala.concurrent.Future
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-object Application extends Controller {
 
-  var db = Database.forConfig("mysql")
+object Application extends Controller {
+  val db = Database.forConfig("mysql")
+
   def index = Action {
-    // val db = Database.forURL("jdbc:mysql://localhost/mydb", driver="com.mysql.jdbc.Driver", user="root", password="")
     val query = sql"SELECT id, username FROM student".as[(Int, String)]
     val f:Future[Vector[(Int, String)]] = db.run(query)
     Await.result(f, Duration.Inf) foreach println
 
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def saka = Action {
+    val query = sql"SELECT id, username FROM student WHERE username = 'sakamoto'".as[(Int, String)]
+    val f:Future[Vector[(Int, String)]] = db.run(query)
+    Await.result(f, Duration.Inf) foreach println
     Ok(views.html.index("Your new application is ready."))
   }
 
