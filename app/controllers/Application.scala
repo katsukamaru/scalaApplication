9ff9060bde63a39e0811e29d17a932a8c95b1d93
shin katsumaru <ms.katsumaru@gmail.com>
2018-03-02 00:42:44 +0900

    separate models package

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,7 @@ import scala.concurrent.Future
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
+import models._
 
 object Application extends Controller {
 
@@ -36,43 +37,17 @@ object Application extends Controller {
   def notImple() = TODO
 
   def listPlaces = Action {
+    implicit val locationWrites: Writes[Location] = (
+      (JsPath \ "lat").write[Double] and (JsPath \ "long").write[Double]) (unlift(Location.unapply))
+
+    implicit val placeWrites: Writes[Place] = (
+      (JsPath \ "name").write[String] and
+        (JsPath \ "location").write[Location]
+      )(unlift(Place.unapply))
+
     val json = Json.toJson(Place.list)
     Ok(json)
   }
-
-  implicit val locationWrites: Writes[Location] = (
-    (JsPath \ "lat").write[Double] and (JsPath \ "long").write[Double]) (unlift(Location.unapply))
-
-  implicit val placeWrites: Writes[Place] = (
-    (JsPath \ "name").write[String] and
-      (JsPath \ "location").write[Location]
-    )(unlift(Place.unapply))
-
-  case class Location(lat: Double, long: Double)
-
-  case class Place(name: String, location: Location)
-
-  object Place {
-
-    var list: List[Place] = {
-      List(
-        Place(
-          "Sandleford",
-          Location(51.377797, -1.318965)
-        ),
-        Place(
-          "Watership Down",
-          Location(51.235685, -1.309197)
-        )
-      )
-    }
-
-    def save(place: Place) = {
-      list = list ::: List(place)
-    }
-  }
-
-
 }
 
 
