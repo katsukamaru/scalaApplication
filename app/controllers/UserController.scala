11fd3f5b4376f3aed030c4a77e1c8e36ec6d9386
shin katsumaru <ms.katsumaru@gmail.com>
2018-03-03 23:44:26 +0900

    push

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api.mvc._
+import slick.jdbc.MySQLProfile.api._
+
+import scala.concurrent.Future
+
+/**
+  * Created by shin on 2018/03/02.
+  */
+object UserController extends Controller {
+  def index = Action {
+    val db = Database.forConfig("mysql")
+    val query = sql"SELECT id, username FROM student".as[(Int, String)]
+    val f:Future[Vector[(Int, String)]] = db.run(query)
+    Ok("ok")
+  }
+}
+
---
+++ memo.md
@@ -0,0 +1,4 @@
+## Slick
+[slick-codegenの利用例と中身の説明](https://esplo.net/2017/12/slick-codegen/)
+
+## 
